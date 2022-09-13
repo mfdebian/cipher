7ae1b839aa29a64f3c6a6bfba50d6c18b7ff5f10
Alfredo González <alfredo.arosteguy@gmail.com>
2022-09-13 11:42:44 -0300

    refactor(*.js): refactor code based on code review, make functions smaller/reusable

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,20 @@
-const cipher = {
-  encode: (offset, string) => {
-    if (!offset || !string || typeof(string) !== "string") {
-      throw new TypeError('Wrong type', "cipher.js", 4)
-    }
-    
-    let encoded = "";
-    Array.from(string).forEach((_, index) => {
-      if(string.charCodeAt(index) >= 65 && string.charCodeAt(index) < 65+26) {
-        encoded += String.fromCharCode((string.charCodeAt(index)-65+offset)%26+65);
-      }
-    });
-    return encoded;
-  },
-  decode: (offset, string) => {
-    if (!offset || !string || typeof(string) !== "string") {
-      throw new TypeError('Wrong type', "cipher.js", 4)
-    }
-
-    let decoded = "";
-    Array.from(string).forEach((_, index) => {
-      if(string.charCodeAt(index) >= 65 && string.charCodeAt(index) <= 65+26) {
-        decoded += String.fromCharCode((string.charCodeAt(index)-65+26-getRangeOffset(offset))%26+65);
-      }
-    });
-    return decoded;
+const encode = (offset, string) => {
+  if (!offset || !string || typeof(string) !== "string") {
+    throw new TypeError('Wrong type', "cipher.js", 2)
   }
+  
+  let encoded = "";
+  Array.from(string).forEach((_, index) => {
+    if(string.charCodeAt(index) >= 65 && string.charCodeAt(index) < 65+26) {
+      encoded += String.fromCharCode((string.charCodeAt(index)-65+offset)%26+65);
+    }
+  });
+  return encoded;
 }
 
-const getRangeOffset = (offset) => {
-  let normalizedOffset = offset%26;
-  return normalizedOffset;
+const cipher = {
+  encode,
+  decode: (offset, string) => encode(26 - (offset % 26), string)
 }
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,32 @@
 import cipher from './cipher.js';
 
-const handleEncode = () => {
-  let encodeResultSpan = document.getElementById("encode-result");
-  encodeResultSpan.textContent = "";
-  let offset = document.getElementById("encode-offset").value;
+const handleEncodeOrDecode = (encodeOrDecode) => {
+  let resultSpan = document.getElementById(`${encodeOrDecode}-result`);
+  resultSpan.textContent = "";
+  let offset = document.getElementById(`${encodeOrDecode}-offset`).value;
   offset = parseInt(offset);
-  let string = document.getElementById("encode-string").value;
-  let encodeErrorParagraph = document.getElementById("encode-error");
-  encodeErrorParagraph.textContent = "";
-  if(offset===0) {
-    encodeErrorParagraph.textContent = "Offset can't be 0";
-  } else if(offset<0) {
-    encodeErrorParagraph.textContent = "Offset can't be negative";
-  } else if(!offset && offset !==0 || !string) {
-    encodeErrorParagraph.textContent = "Inputs can't be empty";
+  let string = document.getElementById(`${encodeOrDecode}-string`).value;
+  let errorParagraph = document.getElementById(`${encodeOrDecode}-error`);
+  errorParagraph.textContent = "";
+  if(offset === 0) {
+    errorParagraph.textContent = "Offset can't be 0";
+  } else if(offset < 0) {
+    errorParagraph.textContent = "Offset can't be negative";
+  } else if(!offset && offset !== 0 || !string) {
+    errorParagraph.textContent = "Inputs can't be empty";
   } else if(string !== string.toUpperCase()) {
-    encodeErrorParagraph.textContent = "Text must be uppercase";
+    errorParagraph.textContent = "Text must be uppercase";
   } else {
-    encodeResultSpan.textContent = cipher.encode(offset, string);
+    if (encodeOrDecode === "encode") {
+      resultSpan.textContent = cipher.encode(offset, string)
+    } else if(encodeOrDecode === "decode") {
+      resultSpan.textContent = cipher.decode(offset, string)
+    }
   }
 }
 
 let encodeButton = document.getElementById("encode-button");
-encodeButton.addEventListener("click", handleEncode, false);
-
-const handleDecode = () => {
-  let decodeResultSpan = document.getElementById("decode-result");
-  decodeResultSpan.textContent = "";
-  let offset = document.getElementById("decode-offset").value;
-  offset = parseInt(offset);
-  let string = document.getElementById("decode-string").value;
-  let decodeErrorParagraph = document.getElementById("decode-error");
-  decodeErrorParagraph.textContent = "";
-  if(offset===0) {
-    decodeErrorParagraph.textContent = "Offset can't be 0";
-  } else if(offset<0) {
-    decodeErrorParagraph.textContent = "Offset can't be negative";
-  } else if(!offset && offset !==0 || !string) {
-    decodeErrorParagraph.textContent = "Inputs can't be empty";
-  } else if(string !== string.toUpperCase()) {
-    decodeErrorParagraph.textContent = "Text must be uppercase";
-  } else {
-    decodeResultSpan.textContent = cipher.decode(offset, string);
-  }
-}
+encodeButton.addEventListener("click", ()=>handleEncodeOrDecode("encode"), false);
 
 let decodeButton = document.getElementById("decode-button");
-decodeButton.addEventListener("click", handleDecode, false);
+decodeButton.addEventListener("click", ()=>handleEncodeOrDecode("decode"), false);
